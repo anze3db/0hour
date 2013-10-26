43af3d6feca477d417fd11b1af3be1a4a19dd35a
Smotko <ensmotko@gmail.com>
2013-10-27 01:45:54 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+angular.module('0hourApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
